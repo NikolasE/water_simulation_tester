837719e7f91f6c55a013e004d529f0786ee37314
Nikolas Engelhard <nikolas.engelhard@in.tum.de>
2012-09-10 18:56:35 +0200

    some comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 /*
  * main.cpp
  *
+ *  This is a demo application to show how an interface to the water_simulation node can look like
+ *
+ *
  *  Created on: Jun 18, 2012
  *      Author: lengelhan
  */
@@ -15,13 +18,12 @@ using namespace std;
 #include <ros/publisher.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
-namespace enc = sensor_msgs::image_encodings;
 
 #include "water_simulation/simulator_init.h"
 #include "water_simulation/simulator_step.h"
 #include "water_simulation/msg_source_sink.h"
 
-
+namespace enc = sensor_msgs::image_encodings;
 
 
 int main(int argc, char ** argv){
@@ -30,26 +32,35 @@ int main(int argc, char ** argv){
 
  ros::NodeHandle nh;
 
- assert(argc > 1);
+ // call with filename as parameter
+ // possible examples can be found in the imgs-folder of the water_simulation stack
+ if (argc <= 1){
+  ROS_INFO("call with path to a grayscale image as parameter");
+  return -1;
+ }
 
  cv::Mat land;
- float scale = 0.4;
+ float scale = 0.4; // don't run simulation on full resolution to speed up
 
  land = cv::imread(argv[1],0);
  cv::resize(land, land, cv::Size(),scale,scale, CV_INTER_CUBIC);
- land.convertTo(land, CV_64FC1,0.2/255);
+ land.convertTo(land, CV_64FC1,0.2/255); // full white corresponds to a height of 0.2m
 
  cv_bridge::CvImage out_msg;
- out_msg.encoding = sensor_msgs::image_encodings::TYPE_64FC1; // Or whatever
+ out_msg.encoding = sensor_msgs::image_encodings::TYPE_64FC1;
  out_msg.image    = land;
 
 
  water_simulation::simulator_init srv_msg;
- srv_msg.request.id = 42;
+ srv_msg.request.id = 42; // set id to some arbitrary value
  srv_msg.request.land_img = *out_msg.toImageMsg();
- srv_msg.request.viscosity = 0.9;
+ srv_msg.request.viscosity = 0.9; // viscosity of simulation in the interval (0,1]
+ srv_msg.request.add_sink_border = true; // water at the border of the grid vanishes
 
 
+ // make sure that the service is available e.g.
+ // > rosrun water_simulation simulation
+ // was called
  ros::service::waitForService("srv_simulator_init",ros::DURATION_MAX);
  ros::service::waitForService("srv_simulator_step",ros::DURATION_MAX);
 
@@ -64,16 +75,19 @@ int main(int argc, char ** argv){
  msg_step.request.id = srv_msg.request.id;
  msg_step.request.iteration_cnt = 50;
 
+
+ // always 0.2 water on this spot
  water_simulation::msg_source_sink source;
- source.height = 0.2;
- source.x = 246;
+ source.height = 0.2; // new height of 20cm
+ source.x = 246; // position in grid
  source.y = 96;
- source.radius = 10;
+ source.radius = 10; // radius of new source
  msg_step.request.sources_sinks.push_back(source);
 
- source.height = 0.0;
+ // remove all water in this area
+ source.height = 0.0; // all water in this area vanishes
  source.x = 200;
- source.radius = 20;
+ source.radius = 20; // large sink
  msg_step.request.sources_sinks.push_back(source);
 
  cv::namedWindow("water");
@@ -81,8 +95,6 @@ int main(int argc, char ** argv){
  cv::Mat water_img;
  ros::Rate r(10);
  while (ros::ok()){
-
-
 
   if (ros::service::call("srv_simulator_step", msg_step)){
 
